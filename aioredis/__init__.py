e11ff8b25155fe49ad2aab89a257118d628a9ecd
Alexey Popravka <alexey.popravka@horsedevel.com>
2014-05-29 01:59:48 +0300

    add setup.py & CHANGES.txt; increment version

--- aioredis/__init__.py
+++ aioredis/__init__.py
@@ -2,7 +2,7 @@ from .connection import RedisConnection, create_connection
 from .errors import RedisError, ProtocolError, ReplyError
 
 
-__version__ = '0.0.0'
+__version__ = '0.0.1'
 
 # make pyflakes happy
 (create_connection, RedisConnection,
---
+++ setup.py
@@ -0,0 +1,60 @@
+import re
+import os.path
+import sys
+from setuptools import setup, find_packages
+
+
+install_requires = ['hiredis']
+
+PY_VER = sys.version_info
+
+if PY_VER >= (3, 4):
+    pass
+elif PY_VER >= (3, 3):
+    install_requires.append('asyncio')
+else:
+    raise RuntimeError("aioredis doesn't support Python version prior 3.3")
+
+
+def read(*parts):
+    with open(os.path.join(*parts), 'rt') as f:
+        return f.read().strip()
+
+def read_version():
+    regexp = re.compile(r"^__version__\W*=\W*'([\d.abrc]+)'")
+    init_py = os.path.join(os.path.dirname(__file__),
+                           'aioredis', '__init__.py')
+    with open(init_py) as f:
+        for line in f:
+            match = regexp.match(line)
+            if match is not None:
+                return match.group(1)
+        else:
+            raise RuntimeError('Cannot find version in aioredis/__init__.py')
+
+
+classifiers = [
+    'License :: OSI Approved :: MIT License',
+    'Development Status :: 3 - Alpha',
+    'Programming Language :: Python',
+    # 'Programming Language :: Python :: 3.3', # TODO: fix unittests for this
+    'Programming Language :: Python :: 3.4',
+    'Operating System :: POSIX',
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+]
+
+setup(name='aioredis',
+      version=read_version(),
+      description=(""),
+      long_description="\n\n".join((read('README.rst'), read('CHANGES.txt'))),
+      classifiers=classifiers,
+      platforms=["POSIX"],
+      author="Alexey Popravka",
+      author_email="alexey.popravka@horsedevel.com",
+      url="https://github.com/popravich/aioredis",
+      license="MIT",
+      packages=find_packages(exclude=["tests"]),
+      install_requires=install_requires,
+      include_package_data=True,
+      )
