ecaf9ef9589ba7a04a4c8d144869f9aa602a464b
Alexey Popravka <alexey.popravka@horsedevel.com>
2014-12-17 00:07:02 +0200

    pyflakes fix

--- tests/string_commands_test.py
+++ tests/string_commands_test.py
@@ -605,3 +605,4 @@ class StringCommandsTest(RedisTest):
         exists_coro = self.redis._conn.execute("EXISTS", b"key:test1")
         exists_coro.cancel()
         exists_check = yield from self.redis.exists(b"key:test2")
+        self.assertFalse(exists_check)
