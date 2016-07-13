c830e92a6068ef31b07ab969cd6dc145ec859ee7
Alexey Popravka <alexey.popravka@horsedevel.com>
2016-07-13 21:15:46 +0300

    update docs: add ghbtns.com

--- docs/conf.py
+++ docs/conf.py
@@ -127,6 +127,11 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
         html_theme = 'sphinx_rtd_theme'
         html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
+html_context = {
+    'github_user': 'aio-libs',
+    'github_repo': 'aioredis',
+    }
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
