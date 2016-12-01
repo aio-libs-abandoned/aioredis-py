59e64ef92f7d941ade10318101759b503bb08fe7
Moe <moe_joe_dev@yahoo.com>
2016-12-01 20:19:35 +0200

    Updating commands.py file for Py3.5 support
    - Commented out Py3.4 code
    - Added async/await syntax for Py3.5 support

--- examples/commands.py
+++ examples/commands.py
@@ -1,7 +1,9 @@
 import asyncio
 import aioredis
 
-
+# The code that is commented out is for Python 3.4 
+# It is being left here for purposes of making it easy to reference Py3.4 vs. Py3.5
+'''
 def main():
     loop = asyncio.get_event_loop()
 
@@ -11,6 +13,21 @@ def main():
             ('localhost', 6379))
         yield from redis.set('my-key', 'value')
         val = yield from redis.get('my-key')
+        print(val)
+
+        # optinally closing underlying connection
+        redis.close()
+    loop.run_until_complete(go())
+ '''
+
+def main():
+    loop = asyncio.get_event_loop()
+
+    async def go():
+        redis = await aioredis.create_redis(
+            ('localhost', 6379))
+        await redis.set('my-key', 'value')
+        val = await redis.get('my-key')
         print(val)
 
         # optinally closing underlying connection
