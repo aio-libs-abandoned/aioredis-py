13050f57110dfaca5da203555e95e3fdd54a1bfb
Alexey Popravka <alexey.popravka@horsedevel.com>
2017-01-06 15:15:58 +0200

    update docs to include geo commands

--- aioredis/commands/geo.py
+++ aioredis/commands/geo.py
@@ -15,41 +15,61 @@ class GeoCommandsMixin:
 
     def geoadd(self, key, longitude, latitude, member, *args, **kwargs):
         """Add one or more geospatial items in the geospatial index represented
-        using a sorted set
+        using a sorted set.
         """
         return self._conn.execute(
             b'GEOADD', key, longitude, latitude, member, *args, **kwargs
         )
 
     def geohash(self, key, member, *args, **kwargs):
-        """Returns members of a geospatial index as standard geohash strings
+        """Returns members of a geospatial index as standard geohash strings.
         """
         return self._conn.execute(
             b'GEOHASH', key, member, *args, **kwargs
         )
 
     def geopos(self, key, member, *args, **kwargs):
-        """Returns longitude and latitude of members of a geospatial index
+        """Returns longitude and latitude of members of a geospatial index.
+
+        :rtype: list[GeoPoint or None]
         """
         fut = self._conn.execute(b'GEOPOS', key, member, *args, **kwargs)
         return wait_convert(fut, make_geopos)
 
     def geodist(self, key, member1, member2, unit='m'):
-        """Returns the distance between two members of a geospatial index
+        """Returns the distance between two members of a geospatial index.
+
+        :rtype: list[float or None]
         """
         fut = self._conn.execute(b'GEODIST', key, member1, member2, unit)
         return wait_convert(fut, make_geodist)
 
-    def georadius(self, key, longitude, latitude, radius, unit='m',
+    def georadius(self, key, longitude, latitude, radius, unit='m', *,
                   with_dist=False, with_hash=False, with_coord=False,
                   count=None, sort=None, encoding=_NOTSET):
         """Query a sorted set representing a geospatial index to fetch members
-        matching a given maximum distance from a point
+        matching a given maximum distance from a point.
+
+        Return value follows Redis convention:
+
+        * if none of ``WITH*`` flags are set -- list of strings returned:
+
+            >>> await redis.georadius('Sicily', 15, 37, 200, 'km')
+            [b"Palermo", b"Catania"]
+
+        * if any flag (or all) is set -- list of named tuples returned:
+
+            >>> await redis.georadius('Sicily', 15, 37, 200, 'km',
+            ...                       with_dist=True)
+            [GeoMember(name=b"Palermo", dist=190.4424, hash=None, coord=None),
+             GeoMember(name=b"Catania", dist=56.4413, hash=None, coord=None)]
 
         :raises TypeError: radius is not float or int
         :raises TypeError: count is not int
-        :raises ValueError: if unit not equal 'm' or 'km' or 'mi' or 'ft'
-        :raises ValueError: if sort not equal 'ASC' or 'DESC'
+        :raises ValueError: if unit not equal ``m``, ``km``, ``mi`` or ``ft``
+        :raises ValueError: if sort not equal ``ASC`` or ``DESC``
+
+        :rtype: list[str] or list[GeoMember]
         """
         args = validate_georadius_options(
             radius, unit, with_dist, with_hash, with_coord, count, sort
@@ -66,16 +86,32 @@ class GeoCommandsMixin:
                                 with_coord=with_coord)
         return fut
 
-    def georadiusbymember(self, key, member, radius, unit='m',
+    def georadiusbymember(self, key, member, radius, unit='m', *,
                           with_dist=False, with_hash=False, with_coord=False,
                           count=None, sort=None, encoding=_NOTSET):
         """Query a sorted set representing a geospatial index to fetch members
-        matching a given maximum distance from a member
+        matching a given maximum distance from a member.
+
+        Return value follows Redis convention:
+
+        * if none of ``WITH*`` flags are set -- list of strings returned:
+
+            >>> await redis.georadiusbymember('Sicily', 'Palermo', 200, 'km')
+            [b"Palermo", b"Catania"]
+
+        * if any flag (or all) is set -- list of named tuples returned:
+
+            >>> await redis.georadiusbymember('Sicily', 'Palermo', 200, 'km',
+            ...                               with_dist=True)
+            [GeoMember(name=b"Palermo", dist=190.4424, hash=None, coord=None),
+             GeoMember(name=b"Catania", dist=56.4413, hash=None, coord=None)]
 
         :raises TypeError: radius is not float or int
         :raises TypeError: count is not int
-        :raises ValueError: if unit not equal 'm' or 'km' or 'mi' or 'ft'
-        :raises ValueError: if sort not equal 'ASC' or 'DESC'
+        :raises ValueError: if unit not equal ``m``, ``km``, ``mi`` or ``ft``
+        :raises ValueError: if sort not equal ``ASC`` or ``DESC``
+
+        :rtype: list[str] or list[GeoMember]
         """
         args = validate_georadius_options(
             radius, unit, with_dist, with_hash, with_coord, count, sort
@@ -104,7 +140,7 @@ def validate_georadius_options(radius, unit, with_dist, with_hash, with_coord,
         args.append(b'WITHCOORD')
 
     if unit not in ['m', 'km', 'mi', 'ft']:
-        raise ValueError("unit argument must be 'm' or 'km' or 'mi' or 'ft'")
+        raise ValueError("unit argument must be 'm', 'km', 'mi' or 'ft'")
     if not isinstance(radius, (int, float)):
         raise TypeError("radius argument must be int or float")
     if count:
