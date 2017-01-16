8ee8b671c8186647f2a79c66bd7cb44d56515a0f
Alexey Popravka <alexey.popravka@horsedevel.com>
2017-01-16 16:44:32 +0200

    get rid of wait_for in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,6 +12,7 @@ import logging
 import tempfile
 
 from collections import namedtuple
+from async_timeout import timeout as async_timeout
 
 import aioredis
 import aioredis.sentinel
@@ -479,11 +480,18 @@ def pytest_pyfunc_call(pyfuncitem):
         loop = funcargs['loop']
         testargs = {arg: funcargs[arg]
                     for arg in pyfuncitem._fixtureinfo.argnames}
+
         loop.run_until_complete(
-            asyncio.wait_for(pyfuncitem.obj(**testargs),
-                             marker.kwargs.get('timeout', 15),
-                             loop=loop))
+            _wait_coro(pyfuncitem.obj, testargs,
+                       timeout=marker.kwargs.get('timeout', 15),
+                       loop=loop))
         return True
+
+
+@asyncio.coroutine
+def _wait_coro(corofunc, kwargs, timeout, loop):
+    with async_timeout(timeout, loop=loop):
+        return (yield from corofunc(**kwargs))
 
 
 def pytest_runtest_setup(item):
