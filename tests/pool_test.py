05784ac1c715d2b0cbd520df739b0bcad74a71f3
Hu Bo <hubo1016@126.com>
2017-05-03 20:02:52 +0800

    Do not return a connection with pending requests to the pool (#210)
    * Close a connection with pending requests
    
    A redis connection cannot return to a pool if it has pending requests, especially commands like "BLPOP/BRPOP". The command may block for an indefinite time, and stops the following commands from executing if the connection is acquired from the pool. Also, because the command is still executing, the BRPOP command may pop an item and drop it which creates bugs which are very hard to debug.
    
    * test closing connection with pending requests
    
    * fix test
    
    * bug fix: use loop in input
    
    * bug fix: parameters
    
    * format

--- tests/pool_test.py
+++ tests/pool_test.py
@@ -152,6 +152,29 @@ def test_release_closed(create_pool, loop, server):
 
 
 @pytest.mark.run_loop
+def test_release_pending(create_pool, loop, server):
+    pool = yield from create_pool(
+        server.tcp_address,
+        minsize=1, loop=loop)
+    assert pool.size == 1
+    assert pool.freesize == 1
+
+    with (yield from pool) as conn:
+        try:
+            yield from asyncio.wait_for(
+                conn.execute(
+                    b'blpop',
+                    b'somekey:not:exists',
+                    b'0'),
+                0.1,
+                loop=loop)
+        except asyncio.TimeoutError:
+            pass
+    assert pool.size == 0
+    assert pool.freesize == 0
+
+
+@pytest.mark.run_loop
 def test_release_bad_connection(create_pool, create_redis, loop, server):
     pool = yield from create_pool(
         server.tcp_address,
