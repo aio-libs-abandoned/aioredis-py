67a199173c4f432ff2d85f8e66d42dc5e2d108ca
hubo <hubo1016@126.com>
2017-05-03 15:25:14 +0800

    format

--- tests/pool_test.py
+++ tests/pool_test.py
@@ -150,7 +150,7 @@ def test_release_closed(create_pool, loop, server):
     assert pool.size == 0
     assert pool.freesize == 0
 
-    
+
 @pytest.mark.run_loop
 def test_release_pending(create_pool, loop, server):
     pool = yield from create_pool(
@@ -161,7 +161,13 @@ def test_release_pending(create_pool, loop, server):
 
     with (yield from pool) as conn:
         try:
-            yield from asyncio.wait_for(conn.execute(b'blpop', b'somekey:not:exists', b'0'), 0.1, loop=loop)
+            yield from asyncio.wait_for(
+                conn.execute(
+                    b'blpop',
+                    b'somekey:not:exists',
+                    b'0'),
+                0.1,
+                loop=loop)
         except asyncio.TimeoutError:
             pass
     assert pool.size == 0
