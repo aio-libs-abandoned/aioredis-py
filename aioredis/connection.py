e38be7203a7338e5b672505d27b8e1394adeebcd
Pau Freixes <pau.freixes@skyscanner.net>
2017-05-11 22:07:33 +0200

    Removed print

--- aioredis/connection.py
+++ aioredis/connection.py
@@ -73,7 +73,6 @@ def create_connection(address, *, db=None, password=None, ssl=None,
     if isinstance(address, (list, tuple)):
         host, port = address
         logger.debug("Creating tcp connection to %r", address)
-        print(asyncio.open_connection)
         reader, writer = yield from asyncio.wait_for(asyncio.open_connection(
             host, port, ssl=ssl, loop=loop), timeout, loop=loop)
         sock = writer.transport.get_extra_info('socket')
