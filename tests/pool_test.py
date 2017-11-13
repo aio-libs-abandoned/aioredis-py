cb78efcc01230f135b03a796f6f5daef73b5a461
Alexey Popravka <popravich@users.noreply.github.com>
2017-11-13 12:33:17 +0200

    move py35_* tests to original modules

--- tests/pool_test.py
+++ tests/pool_test.py
@@ -531,3 +531,25 @@ async def test_pool_idle_close(create_pool, start_server, loop):
     await asyncio.sleep(2, loop=loop)
 
     assert (await conn.execute('ping')) == b'PONG'
+
+
+@pytest.mark.run_loop
+async def test_await(create_pool, server, loop):
+    pool = await create_pool(
+        server.tcp_address,
+        minsize=10, loop=loop)
+
+    with await pool as conn:
+        msg = await conn.execute('echo', 'hello')
+        assert msg == b'hello'
+
+
+@pytest.mark.run_loop
+async def test_async_with(create_pool, server, loop):
+    pool = await create_pool(
+        server.tcp_address,
+        minsize=10, loop=loop)
+
+    async with pool.get() as conn:
+        msg = await conn.execute('echo', 'hello')
+        assert msg == b'hello'
--- tests/pubsub_receiver_test.py
+++ tests/pubsub_receiver_test.py
@@ -276,3 +276,55 @@ async def test_decode_message_for_pattern(loop):
     res = await mpsc.get(encoding='utf-8', decoder=json.loads)
     assert isinstance(res[0], _Sender)
     assert res[1] == (b'channel', {'hello': 'world'})
+
+
+@pytest.mark.run_loop
+async def test_pubsub_receiver_iter(create_redis, server, loop):
+    sub = await create_redis(server.tcp_address, loop=loop)
+    pub = await create_redis(server.tcp_address, loop=loop)
+
+    mpsc = Receiver(loop=loop)
+
+    async def coro(mpsc):
+        lst = []
+        async for msg in mpsc.iter():
+            lst.append(msg)
+        return lst
+
+    tsk = asyncio.ensure_future(coro(mpsc), loop=loop)
+    snd1, = await sub.subscribe(mpsc.channel('chan:1'))
+    snd2, = await sub.subscribe(mpsc.channel('chan:2'))
+    snd3, = await sub.psubscribe(mpsc.pattern('chan:*'))
+
+    subscribers = await pub.publish_json('chan:1', {'Hello': 'World'})
+    assert subscribers > 1
+    subscribers = await pub.publish_json('chan:2', ['message'])
+    assert subscribers > 1
+    loop.call_later(0, mpsc.stop)
+    # await asyncio.sleep(0, loop=loop)
+    assert await tsk == [
+        (snd1, b'{"Hello": "World"}'),
+        (snd3, (b'chan:1', b'{"Hello": "World"}')),
+        (snd2, b'["message"]'),
+        (snd3, (b'chan:2', b'["message"]')),
+        ]
+    assert not mpsc.is_active
+
+
+@pytest.mark.run_loop(timeout=5)
+async def test_pubsub_receiver_call_stop_with_empty_queue(
+        create_redis, server, loop):
+    sub = await create_redis(server.tcp_address, loop=loop)
+
+    mpsc = Receiver(loop=loop)
+
+    # FIXME: currently at least one subscriber is needed
+    snd1, = await sub.subscribe(mpsc.channel('chan:1'))
+
+    now = loop.time()
+    loop.call_later(.5, mpsc.stop)
+    async for i in mpsc.iter():  # noqa (flake8 bug with async for)
+        assert False, "StopAsyncIteration not raised"
+    dt = loop.time() - now
+    assert dt <= 1.5
+    assert not mpsc.is_active
