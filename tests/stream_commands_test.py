bec4410671f2fef5c42222ea5922dde8748094f5
Adam Charnock <adam@adamcharnock.com>
2017-12-04 14:44:28 +0000

    Streams PR: Fixing use of asyncio in tests for recent changes in aioredis project

--- tests/stream_commands_test.py
+++ tests/stream_commands_test.py
@@ -22,21 +22,21 @@ def skip_if_streams_not_present(server_bin):
 
 
 @asyncio.coroutine
-def add_message_with_sleep(redis, loop, stream, fields):
-    yield from asyncio.sleep(0.2, loop=loop)
-    result = yield from redis.xadd(stream, fields)
+async def add_message_with_sleep(redis, loop, stream, fields):
+    await asyncio.sleep(0.2, loop=loop)
+    result = await redis.xadd(stream, fields)
     return result
 
 
 @pytest.mark.run_loop
-def test_xadd(redis, server_bin):
+async def test_xadd(redis, server_bin):
     skip_if_streams_not_present(server_bin)
 
     fields = OrderedDict((
         (b'field1', b'value1'),
         (b'field2', b'value2'),
     ))
-    message_id = yield from redis.xadd('test_stream', fields)
+    message_id = await redis.xadd('test_stream', fields)
 
     # Check the result is in the expected format (i.e: 1507400517949-0)
     assert b'-' in message_id
@@ -45,7 +45,7 @@ def test_xadd(redis, server_bin):
     assert sequence.isdigit()
 
     # Read it back
-    messages = yield from redis.xrange('test_stream')
+    messages = await redis.xrange('test_stream')
     assert len(messages) == 1
     message = messages[0]
     assert message[0] == message_id
@@ -56,7 +56,7 @@ def test_xadd(redis, server_bin):
 
 
 @pytest.mark.run_loop
-def test_xrange(redis, server_bin):
+async def test_xrange(redis, server_bin):
     skip_if_streams_not_present(server_bin)
 
     stream = 'test_stream'
@@ -64,12 +64,12 @@ def test_xrange(redis, server_bin):
         (b'field1', b'value1'),
         (b'field2', b'value2'),
     ))
-    message_id1 = yield from redis.xadd(stream, fields)
-    message_id2 = yield from redis.xadd(stream, fields)
-    message_id3 = yield from redis.xadd(stream, fields)  # noqa
+    message_id1 = await redis.xadd(stream, fields)
+    message_id2 = await redis.xadd(stream, fields)
+    message_id3 = await redis.xadd(stream, fields)  # noqa
 
     # Test no parameters
-    messages = yield from redis.xrange(stream)
+    messages = await redis.xrange(stream)
     assert len(messages) == 3
     message = messages[0]
     assert message[0] == message_id1
@@ -79,40 +79,40 @@ def test_xrange(redis, server_bin):
     )
 
     # Test start
-    messages = yield from redis.xrange(stream, start=message_id2)
+    messages = await redis.xrange(stream, start=message_id2)
     assert len(messages) == 2
 
-    messages = yield from redis.xrange(stream, start='9900000000000-0')
+    messages = await redis.xrange(stream, start='9900000000000-0')
     assert len(messages) == 0
 
     # Test stop
-    messages = yield from redis.xrange(stream, stop='0000000000000-0')
+    messages = await redis.xrange(stream, stop='0000000000000-0')
     assert len(messages) == 0
 
-    messages = yield from redis.xrange(stream, stop=message_id2)
+    messages = await redis.xrange(stream, stop=message_id2)
     assert len(messages) == 2
 
-    messages = yield from redis.xrange(stream, stop='9900000000000-0')
+    messages = await redis.xrange(stream, stop='9900000000000-0')
     assert len(messages) == 3
 
     # Test start & stop
-    messages = yield from redis.xrange(stream,
+    messages = await redis.xrange(stream,
                                        start=message_id1,
                                        stop=message_id2)
     assert len(messages) == 2
 
-    messages = yield from redis.xrange(stream,
+    messages = await redis.xrange(stream,
                                        start='0000000000000-0',
                                        stop='9900000000000-0')
     assert len(messages) == 3
 
     # Test count
-    messages = yield from redis.xrange(stream, count=2)
+    messages = await redis.xrange(stream, count=2)
     assert len(messages) == 2
 
 
 @pytest.mark.run_loop
-def test_xread_selection(redis, server_bin):
+async def test_xread_selection(redis, server_bin):
     """Test use of counts and starting IDs"""
     skip_if_streams_not_present(server_bin)
 
@@ -121,33 +121,33 @@ def test_xread_selection(redis, server_bin):
         (b'field1', b'value1'),
         (b'field2', b'value2'),
     ))
-    message_id1 = yield from redis.xadd(stream, fields)
-    message_id2 = yield from redis.xadd(stream, fields)  # noqa
-    message_id3 = yield from redis.xadd(stream, fields)
+    message_id1 = await redis.xadd(stream, fields)
+    message_id2 = await redis.xadd(stream, fields)  # noqa
+    message_id3 = await redis.xadd(stream, fields)
 
-    messages = yield from redis.xread([stream],
+    messages = await redis.xread([stream],
                                       timeout=1,
                                       latest_ids=['0000000000000-0'])
     assert len(messages) == 3
 
-    messages = yield from redis.xread([stream],
+    messages = await redis.xread([stream],
                                       timeout=1,
                                       latest_ids=[message_id1])
     assert len(messages) == 2
 
-    messages = yield from redis.xread([stream],
+    messages = await redis.xread([stream],
                                       timeout=1,
                                       latest_ids=[message_id3])
     assert len(messages) == 0
 
-    messages = yield from redis.xread([stream],
+    messages = await redis.xread([stream],
                                       timeout=1,
                                       latest_ids=['0000000000000-0'], count=2)
     assert len(messages) == 2
 
 
 @pytest.mark.run_loop
-def test_xread_blocking(redis, create_redis, loop, server, server_bin):
+async def test_xread_blocking(redis, create_redis, loop, server, server_bin):
     """Test the blocking read features"""
     skip_if_streams_not_present(server_bin)
 
@@ -155,7 +155,7 @@ def test_xread_blocking(redis, create_redis, loop, server, server_bin):
         (b'field1', b'value1'),
         (b'field2', b'value2'),
     ))
-    other_redis = yield from create_redis(
+    other_redis = await create_redis(
         server.tcp_address, loop=loop)
 
     # create blocking task in separate connection
@@ -163,7 +163,7 @@ def test_xread_blocking(redis, create_redis, loop, server, server_bin):
 
     producer_task = asyncio.Task(
         add_message_with_sleep(redis, loop, 'test_stream', fields), loop=loop)
-    results = yield from asyncio.gather(
+    results = await asyncio.gather(
         consumer, producer_task, loop=loop)
 
     received_messages, sent_message_id = results
@@ -178,7 +178,7 @@ def test_xread_blocking(redis, create_redis, loop, server, server_bin):
     assert fields == received_fields
 
     # Test that we get nothing back from an empty stream
-    results = yield from redis.xread(['another_stream'], timeout=100)
+    results = await redis.xread(['another_stream'], timeout=100)
     assert results == []
 
     other_redis.close()
