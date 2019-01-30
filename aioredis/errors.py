0044d57301bf7b61e3e2c63ea72e9420c80af10c
Grygorii Iermolenko <gyermolenko@gmail.com>
2019-01-30 12:53:43 +0200

    Add _invalid password_ to AuthError error matches

--- aioredis/errors.py
+++ aioredis/errors.py
@@ -47,7 +47,11 @@ class MaxClientsError(ReplyError):
 class AuthError(ReplyError):
     """Raised when authentication errors occurs."""
 
-    MATCH_REPLY = ("NOAUTH ", "ERR invalid password")
+    MATCH_REPLY = (
+        "NOAUTH ",
+        "ERR invalid password",
+        "ERR Client sent AUTH, but no password is set",
+    )
 
 
 class BusyGroupError(ReplyError):
