6b727af3c9d43faf182d7d6daab6cd7c1b307cf0
Oleg Butuzov <butuzov@made.ua>
2019-04-20 17:46:14 +0300

    Switch to python 3.5 typings style
    Ensuring that typings will be recognized by python 3.5

--- aioredis/parser.py
+++ aioredis/parser.py
@@ -49,13 +49,13 @@ class Parser:
     def __init__(self, protocolError: Callable,
                  replyError: Callable, encoding: Optional[str] ):
 
-        self.buf: bytearray = bytearray()
-        self.pos: int = 0
-        self.protocolError: Callable = protocolError
-        self.replyError: Callable = replyError
-        self.encoding: Optional[str] = encoding
+        self.buf = bytearray()  # type: bytearray
+        self.pos = 0  # type: int
+        self.protocolError = protocolError  # type: Callable
+        self.replyError = replyError  # type: Callable
+        self.encoding = encoding  # type: Optional[str]
         self._err = None
-        self._gen: Optional[Generator] = None
+        self._gen = None # type: Optional[Generator]
 
     def waitsome(self, size: int) -> Iterator[bool]:
         # keep yielding false until at least `size` bytes added to buf.
