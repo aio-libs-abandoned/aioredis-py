ca6e5d0ad7d25529bacb21b96519db54f85e2b68
Alexey Popravka <popravich@users.noreply.github.com>
2019-06-18 13:51:52 +0300

    use patched asyncio.Lock only for Python < 3.7

--- aioredis/locks.py
+++ aioredis/locks.py
@@ -1,44 +1,49 @@
 import asyncio
+import sys
 
-from asyncio.locks import Lock as _Lock
-from asyncio import coroutine
+__all__ = ['Lock']
 
-# Fixes an issue with all Python versions that leaves pending waiters
-# without being awakened when the first waiter is canceled.
-# Code adapted from the PR https://github.com/python/cpython/pull/1031
-# Waiting once it is merged to make a proper condition to relay on
-# the stdlib implementation or this one patched
+if sys.version_info < (3, 7, 0):
+    from asyncio.locks import Lock as _Lock
+    from asyncio import coroutine
 
+    # Fixes an issue with all Python versions that leaves pending waiters
+    # without being awakened when the first waiter is canceled.
+    # Code adapted from the PR https://github.com/python/cpython/pull/1031
+    # Waiting once it is merged to make a proper condition to relay on
+    # the stdlib implementation or this one patched
 
-class Lock(_Lock):
+    class Lock(_Lock):
 
-    @coroutine
-    def acquire(self):
-        """Acquire a lock.
-        This method blocks until the lock is unlocked, then sets it to
-        locked and returns True.
-        """
-        if not self._locked and all(w.cancelled() for w in self._waiters):
-            self._locked = True
-            return True
+        @coroutine
+        def acquire(self):
+            """Acquire a lock.
+            This method blocks until the lock is unlocked, then sets it to
+            locked and returns True.
+            """
+            if not self._locked and all(w.cancelled() for w in self._waiters):
+                self._locked = True
+                return True
 
-        fut = self._loop.create_future()
+            fut = self._loop.create_future()
 
-        self._waiters.append(fut)
-        try:
-            yield from fut
-            self._locked = True
-            return True
-        except asyncio.CancelledError:
-            if not self._locked:  # pragma: no cover
-                self._wake_up_first()
-            raise
-        finally:
-            self._waiters.remove(fut)
+            self._waiters.append(fut)
+            try:
+                yield from fut
+                self._locked = True
+                return True
+            except asyncio.CancelledError:
+                if not self._locked:  # pragma: no cover
+                    self._wake_up_first()
+                raise
+            finally:
+                self._waiters.remove(fut)
 
-    def _wake_up_first(self):
-        """Wake up the first waiter who isn't cancelled."""
-        for fut in self._waiters:
-            if not fut.done():
-                fut.set_result(True)
-                break
+        def _wake_up_first(self):
+            """Wake up the first waiter who isn't cancelled."""
+            for fut in self._waiters:
+                if not fut.done():
+                    fut.set_result(True)
+                    break
+else:
+    from asyncio import Lock
