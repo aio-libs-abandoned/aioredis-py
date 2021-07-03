4065ab42b352977d94b8e71b548728ab1ef84dc9
Andrew-Chen-Wang <acwangpython@gmail.com>
2021-07-03 11:02:09 -0400

    Fix test_acl_getuser_setuser

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -120,7 +120,8 @@ class TestRedisCommands:
         assert await r.acl_setuser(username, enabled=False, reset=True)
         assert await r.acl_getuser(username) == {
             "categories": ["-@all"],
-            "commands": [b"*"],
+            "commands": [],
+            "channels": [b"*"],
             "enabled": False,
             "flags": ["off", "allchannels", "sanitize-payload"],
             "keys": [],
