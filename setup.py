9cf2643f7728efbea3e8a7fc5898881ceef3f4d4
Andrew Brookins <andrew.brookins@redislabs.com>
2021-07-29 11:43:54 -0700

    Bump the version for 2.0.0 release

--- setup.py
+++ setup.py
@@ -27,6 +27,8 @@ classifiers = [
     "Programming Language :: Python :: 3",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3 :: Only",
     "Operating System :: POSIX",
     "Environment :: Web Environment",
