e41b41c09daa42ae74155ff223e3dcbc52efe799
Andrew-Chen-Wang <acwangpython@gmail.com>
2021-10-04 16:01:07 -0400

    Add andymccurdy/redis-py@627db540acd1f1f36db88290d74cbcd75f6bda0c hrandfield (andymccurdy/redis-py#1513)
    Signed-off-by: Andrew-Chen-Wang <acwangpython@gmail.com>

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -962,6 +962,19 @@ class TestRedisCommands:
         """PTTL on servers 2.8 and after return -2 when the key doesn't exist"""
         assert await r.pttl("a") == -2
 
+    @skip_if_server_version_lt("6.2.0")
+    def test_hrandfield(self, r):
+        assert await r.hrandfield("key") is None
+        await r.hset("key", mapping={"a": 1, "b": 2, "c": 3, "d": 4, "e": 5})
+        assert await r.hrandfield("key") is not None
+        assert len(await r.hrandfield("key", 2)) == 2
+        # with values
+        assert len(await r.hrandfield("key", 2, True)) == 4
+        # without duplications
+        assert len(await r.hrandfield("key", 10)) == 5
+        # with duplications
+        assert len(await r.hrandfield("key", -10)) == 10
+
     async def test_randomkey(self, r: aioredis.Redis):
         assert await r.randomkey() is None
         for key in ("a", "b", "c"):
