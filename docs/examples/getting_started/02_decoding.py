0fc15f432dba4499595a072317cd11462d21963a
Filip Hanes <filip@hanes.tech>
2021-10-08 16:31:01 +0200

    fixed docs/example/getting_started/02_decoding.py

--- docs/examples/getting_started/02_decoding.py
+++ docs/examples/getting_started/02_decoding.py
@@ -4,11 +4,11 @@ import aioredis
 
 
 async def main():
-    redis = aioredis.from_url("redis://localhost")
+    redis = aioredis.from_url("redis://localhost", decode_responses=True)
 
-    await redis.hmset_dict("hash", key1="value1", key2="value2", key3=123)
+    await redis.hset("hash", mapping={"key1":"value1", "key2":"value2", "key3":123})
 
-    result = await redis.hgetall("hash", encoding="utf-8")
+    result = await redis.hgetall("hash")
     assert result == {
         "key1": "value1",
         "key2": "value2",
